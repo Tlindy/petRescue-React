7aca6ed472a14ec945679d33466bb3681324ab3b
Teresa L <tml2009jghs@yahoo.com>
2022-03-20 17:03:52 -0400

    Fixed scroll to top of page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Main from "./components/MainComponent";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import "./App.css";
 
 function App() {
     return (
         <BrowserRouter>
-            <div className="App">
-                <Main />
-            </div>
+            <ScrollToTop>
+                <div className="App">
+                    <Main />
+                </div>
+            </ScrollToTop>
         </BrowserRouter>
     );
 }
